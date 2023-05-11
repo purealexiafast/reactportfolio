7efc2bd3e8779ef85f692461a1e9056acd2bebfc
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-05-11 15:44:15 -0700

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navigation from './components/Navigation'
 import Header from './components/Header'
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,9 +1,9 @@
-// import horiseon from "../../images/horiseon.jpg"
-// import portfolio from "../../images/portfolio.jpg"
-// import password from "../../images/password.jpg"
-// import quiz from "../../images/quiz.jpg"
-// import calendar from "../../images/calendar.jpg"
-// import weather from "../../images/weather.jpg"
+import horiseon from "../assets/images/horiseon.jpg"
+import portfolio from "../assets/images/portfolio.jpg"
+import password from "../assets/images/password.jpg"
+import quiz from "../assets/images/quiz.jpg"
+import calendar from "../assets/images/calendar.jpg"
+import weather from "../assets/images/weather.jpg"
 
 
 
@@ -15,37 +15,37 @@ function Portfolio() {
             <p>Please see various projects below.</p>
 
             <h4>Horiseon Website</h4>
-            {<img class="images" src="./horiseon.JPG"></img>}
+            {<img class="images" src={horiseon}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/horiseon-corporation-website"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/horiseon-corporation-website/"><h4>Deployed</h4></a>
             </div>
 
             <h4></h4>
-            {<img class="images" src="./portfolio.JPG"></img>}
+            {<img class="images" src={portfolio}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/alexia-fast-portfolio-project-website"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/alexia-fast-portfolio/"><h4>Deployed</h4></a>
             </div>
 
             <h4></h4>
-            {<img class="images" src="./password.JPG"></img>}
+            {<img class="images" src={password}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/javascript-password-generator"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/javascript-project/"><h4>Deployed</h4></a>
             </div>
 
             <h4></h4>
-            {<img class="images" src="./quiz.JPG"></img>}
+            {<img class="images" src={quiz}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/timed-quiz-javascript"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/timed-quiz-javascript/"><h4>Deployed</h4></a>
             </div>
 
             <h4></h4>
-            {<img class="images" src="./calendar.JPG"></img>}
+            {<img class="images" src={calendar}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/javascript-calender"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/javascript-calender/"><h4>Deployed</h4></a>
             </div>
 
             <h4></h4>
-            {<img class="images" src="./weather.JPG"></img>}
+            {<img class="images" src={weather}></img>}
             <div class="links">
                 <a href="https://github.com/purealexiafast/weather-dash"><h4>GitHub</h4></a><a href="https://purealexiafast.github.io/weather-dash/"><h4>Deployed</h4></a>
             </div>
